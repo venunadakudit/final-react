8423c71e741f622044942b0dc9301d5176bff2eb
venu <venunadakuditi779@gmail.com>
2024-03-17 22:47:29 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,22 @@
+const heading = React.createElement(
+    "div",
+    { id: "parent" },
+    [
+        React.createElement("div", { id: "child" },
+            [
+                React.createElement("h1", { id: "heading1" }, "Hello world in the First heading"),
+                React.createElement("h2", { id: "heading2" }, "Hello world in the First heading")
+            ]
+        ),
+        React.createElement("div", { id: "child" },
+            [
+                React.createElement("h3", { id: "heading3" }, "Hello world in the First heading"),
+                React.createElement("h4", { id: "heading4" }, "Hello world in the First heading")
+            ]
+        )
+    ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
