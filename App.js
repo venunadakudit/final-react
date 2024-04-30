0935b8172de40b8fe55743559c2ac2d48899945e
venu <venunadakuditi779@gmail.com>
2024-04-30 23:07:35 +0530

    igniting our app

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const heading = React.createElement(
     "div",
     { id: "parent" },
     [
         React.createElement("div", { id: "child" },
             [
-                React.createElement("h1", { id: "heading1" }, "Hello world in the First heading"),
+                React.createElement("h1", { id: "heading1" }, "Hello world in the venu component"),
                 React.createElement("h2", { id: "heading2" }, "Hello world in the First heading")
             ]
         ),
---
+++ README.md
@@ -0,0 +1,24 @@
+# Namasthe React
+
+
+# parcel
+- Dev Build
+- Local server
+-HMR = Hot Module Replacement
+-File Watching Algorithm - Written in C++
+-Caching - Faster Builds
+-Image optimization
+-Minification
+-Bundling
+-Compress
+-Consistent Hashing
+-Code Slitting 
+-Differential Bundling - Support older brtowsers.
+-Diagnostic
+-Error Handling
+-HTTPs
+-Tree shaking - remove unused code 
+-Difference dev and prod bundles
+
+
+
