9a15a0857590fa4bf2a60525f9ce3dcff363e74f
venunadakuditi <vnadakuditi@appstekcorp.com>
2024-06-03 19:05:25 +0530

    Card data is dynamic.

--- App.js
+++ App.js
@@ -33,14 +33,15 @@ const Header = () => {
     )
 }
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+    console.log(props);
     return(
         <div className="res-card">
             <img className="res-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/63178e3e64d503a479f2a2048a474552" />
-           <h3>Meghana Food</h3>
-           <h4>Biryani, North Indian, Asian</h4>
-           <h4>4.4 Stars</h4>
-           <h4>38 Minutes</h4>
+           <h3>{props.resName}</h3>
+           <h4>{props.cuisine}</h4>
+           <h4>{props.resRating} Stars</h4>
+           <h4>{props.resTime}</h4>
         </div>
     )
 }
@@ -52,12 +53,9 @@ const Body = () => {
                 <button className="search-btn">Search</button>
             </div>
             <div className="restaurant-container">
-                  <RestaurantCard />
-                  <RestaurantCard />
-                  <RestaurantCard />
-                  <RestaurantCard />
-                  <RestaurantCard />
-                  <RestaurantCard /> 
+                  <RestaurantCard resName="Meghana Food" resRating="4.4 Stars" resTime="38 Minutes" cuisine="Biryani, North Indian, Asian"/>
+                  <RestaurantCard resName="KFC" resRating="4.6 Stars" resTime="48 Minutes" cuisine="Burger, Fast Food"/>
+                 
             </div>
         </div>
     )
