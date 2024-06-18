ab35f5bcded1de7ff85a3227a0836add088f557a
venunadakuditi <vnadakuditi@appstekcorp.com>
2024-06-18 18:55:56 +0530

    Create login and logout btn with state variable. shimmer ternary operater condition added.

--- src/components/Body.js
+++ src/components/Body.js
@@ -20,11 +20,11 @@ const Body = () => {
     setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants); 
   };
 
-  if(listOfRestaurants.length === 0){
-    return <Shimmer />; 
-  }
+  // if(listOfRestaurants.length === 0){
+  //   return <Shimmer />; 
+  // }
 
-  return ( 
+  return listOfRestaurants.length === 0 ? <Shimmer /> : ( 
     <div className="body">
       <div className="filter">
         <button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 const Header = () => {
+ const [btnNameReact, setBtnNameReact] = useState('Login');  
+ console.log('Render header'); 
   return (
     <div className="header">
       <div className="logo-container">
@@ -11,6 +14,13 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button className="login-btn"
+          onClick={() => {
+            btnNameReact === 'Login' ? setBtnNameReact('Logout') : setBtnNameReact('Login'); 
+          }}
+          >
+              {btnNameReact}
+              </button>
         </ul>
       </div>
     </div>
