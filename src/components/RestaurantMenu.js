a7022cc72bc4955a502535d62bcdc42898af413b
venunadakuditi <vnadakuditi@appstekcorp.com>
2025-06-14 14:09:54 +0530

    Creating an custom Hook(useRestaurentMenu). custom Hook means, extracted fetching data from API. that code moved from restaurentMenu to custom Hook.

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,21 +1,22 @@
-import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import {MENU_API} from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
+  // const [resInfo, setResInfo] = useState(null);
  
   const {resId} = useParams(); 
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
+  const resInfo = useRestaurantMenu(resId);
 
-  const fetchMenu = async () => {
-    const data = await fetch( MENU_API + resId);
-    const json = await data.json(); 
-    setResInfo(json.data);
-  };
+  // useEffect(() => {
+  //   fetchMenu();
+  // }, []);
+
+  // const fetchMenu = async () => {
+  //   const data = await fetch( MENU_API + resId);
+  //   const json = await data.json(); 
+  //   setResInfo(json.data);
+  // };
   
   if (resInfo === null) return <Shimmer />;
  
